7f259c3d8d5e756b41762a1e62598ba964a399a8
Administrator <jcsmnd@gmail.com>
2019-01-30 00:58:25 -0500

    Add google interview coding sample question 01

--- src/JavaBasic01.java
+++ src/JavaBasic01.java
@@ -1,17 +1,35 @@
-// written by Myungsik Kim 01/29/2019
-// OOP - Encapsulation
+// written by Myungsik Kim 01/29/2019.
+// OOP - Constructor, Encapsulation, Information Hiding, Controlling Access to Members of a Class, and Inheritance.
 
-public class JavaBasic01 {
+//There are no package declared. So all down below classes are belong to "default" package.
+
+public class JavaBasic01 { // you can call this "public" class in any java classes.
     public static void main(String[] args) {
         Bank citi = new Bank(2019001);
         System.out.println(citi.getId());
+        citi.comment = "Mr. Kim's Account";
+        System.out.println(citi.comment);
+
+        Clerk kim = new Clerk();
+        kim.clerkId(1101);
+        System.out.println(kim.clerkId);
+
+        kim.IsUserActive(true);
+        System.out.println(kim.active_user);
     }
 }
 
-class Bank {
-    private int id = 0;
+class Bank { // you can call this "default" class in only same package.
+    private int id = 0; // Private: It can be accessed from only under "Bank" class and able to use this variable.
+    public String comment = ""; // Public: It can be accessed from anywhere.
+    int clerkId = 0; // Default: It can be accessed from in same Package.
+    protected boolean active_user = false; //even if child class have different package, "protected" allows to use this variable.
 
-    public Bank(int i) {
+    public Bank(){ // Default constructor
+
+    }
+
+    public Bank(int i) { // Parameterized Constructor
         setId(i);
     }
 
@@ -27,3 +45,19 @@ class Bank {
         return id;
     }
 }
+
+class Clerk extends Bank { //Clerk is a child class and inherits from the parent class Bank.
+    public void clerkId(int i){
+        clerkId = i; //In this case class Clerk and Bank are belong to default package. That's why class Clerk can use clerkId variable.
+    }
+
+    public void IsUserActive(boolean b){
+        active_user = b; //If Clerk and Bank classes are belong to different package, protected (Line 26) allows to use it in child class.
+    }
+}
+
+/* You can't use more than two "public" class in same java class.
+public class JavaBasic02{
+
+}
+*/
