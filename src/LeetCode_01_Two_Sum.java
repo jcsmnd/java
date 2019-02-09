199f50c0257a964a1bc8abc36da107fc2798def5
Administrator <jcsmnd@gmail.com>
2019-02-09 12:35:57 -0500

    LeetCocde 01

--- src/LeetCode_01_Two_Sum.java
+++ src/LeetCode_01_Two_Sum.java
@@ -22,7 +22,7 @@ public class LeetCode_01_Two_Sum {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
+                    return new int[] {i,j};
                 }
             }
         }
